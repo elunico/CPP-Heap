4d40c317c68f23771489d19472ff03bc9f50140d
Thomas Povinelli <tompov227@gmail.com>
2021-07-15 18:50:42 +0000

    Initial commit

---
+++ heap.h
@@ -0,0 +1,129 @@
+//
+// Created by Thomas Povinelli on 7/13/21.
+//
+
+#ifndef MAXHEAP_HEAP_H
+#define MAXHEAP_HEAP_H
+
+#include <cassert>
+#include <cstddef>
+#include <functional>
+
+namespace tom {
+
+template <typename T> static void aswap(T arr[], int a, int b) {
+  T const val = arr[a];
+  arr[a] = arr[b];
+  arr[b] = val;
+}
+
+template <typename T> using comparator_func = int (*)(T, T);
+
+template <typename T> class heap {
+
+  comparator_func<T> comparator;
+  std::size_t count_ = 0;
+  std::size_t capacity = 0;
+  T *data = nullptr;
+
+  void heapify() {
+    std::size_t index = count_ - 1;
+    while (index > 0) {
+      std::size_t parent = (index - 1) / 2;
+
+      if (parent < capacity && comparator(data[index], data[parent]) > 0) {
+        aswap(data, index, parent);
+        index = parent;
+      } else {
+        break;
+      }
+    }
+  }
+
+  void sift_down(std::size_t idx) noexcept {
+    std::size_t child1 = (idx * 2) + 1;
+    std::size_t child2 = (idx * 2) + 2;
+
+    if (child1 < count_ && comparator(data[child1], data[idx]) > 0) {
+      aswap(data, child1, idx);
+      sift_down(child1);
+    }
+    if (child2 < count_ && comparator(data[child2], data[idx]) > 0) {
+      aswap(data, child2, idx);
+      sift_down(child2);
+    }
+  }
+
+  void resize(std::size_t target) {
+    auto newData = new T[target]();
+    std::copy(begin(), end(), newData);
+    if (data != nullptr)
+      delete[] data;
+    data = newData;
+    capacity = target;
+  }
+
+  void grow() { resize((std::size_t)((capacity << 1) + 1)); }
+
+public:
+  heap() noexcept : heap(std::greater<T>{}) {}
+
+  explicit heap(int (*comparator)(T, T)) noexcept : comparator(comparator) {}
+
+  void put(T t) {
+    if (count_ == capacity) {
+      grow();
+    }
+    assert(count_ < capacity);
+    data[count_++] = t;
+    heapify();
+  }
+
+  T const &peek() const noexcept {
+    assert(capacity > 0);
+    return data[0];
+  }
+
+  T &peek() noexcept {
+    assert(capacity > 0);
+    return data[0];
+  }
+
+  T pop() {
+    assert(count_ - 1 >= 0);
+
+    aswap(data, 0, count_ - 1);
+    T value = data[count_ - 1];
+    count_--;
+    sift_down(0);
+    return value;
+  }
+
+  void reserve(std::size_t capacity) {
+    if (capacity > 0)
+      resize(capacity);
+  }
+
+  auto *begin() noexcept { return data; }
+
+  auto const *begin() const noexcept { return data; }
+
+  auto *end() noexcept { return data + count_; }
+
+  auto const *end() const noexcept { return data + count_; }
+
+  [[nodiscard]] std::size_t count() const noexcept { return count_; }
+
+  static heap max_heap() {
+    return heap{+[](T a, T b) { return a > b ? 1 : (a == b ? 0 : -1); }};
+  }
+
+  static heap min_heap() {
+    return heap{+[](T a, T b) { return a > b ? 1 : (a == b ? 0 : -1); }};
+  }
+
+  ~heap() noexcept { delete[] data; }
+};
+
+} // namespace tom
+#endif // MAXHEAP_HEAP_H
---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include "heap.h"
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+void dump_heap(tom::heap<int> const &heap) {
+  for (auto const &a : heap)
+    std::cout << a << " ";
+  std::cout << std::endl;
+}
+
+struct Vector3D {
+  double x, y, z;
+
+  long long a, b;
+
+  std::array<double *, 10> arr;
+
+  int operator>(Vector3D const &other) {
+    return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)) >
+                   sqrt(pow(other.x, 2) + pow(other.y, 2) + pow(other.z, 2))
+               ? 1
+               : -1;
+  }
+
+  int operator==(Vector3D const &other) {
+    return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)) ==
+           sqrt(pow(other.x, 2) + pow(other.y, 2) + pow(other.z, 2));
+  }
+};
+
+int main() {
+
+  auto heap = tom::heap<Vector3D>::max_heap();
+  for (int i = 0; i < 500000; i++)
+    heap.put(Vector3D{rand() / (double)RAND_MAX, rand() / (double)RAND_MAX,
+                      rand() / (double)RAND_MAX});
+
+  // std::vector<int> v{};
+  // for (int i = 0; i < 500000; i++)
+  //   v.push_back(i);
+
+  //   size_t count = heap.count();
+
+  //   for (int i = 0; i < count; i++) {
+  //     auto a = heap.pop();
+  //     if (a % 351 == 0)
+  //       std::cout << a << " " << std::endl;
+  //   }
+  std::cout << std::endl;
+
+  return 0;
+}
