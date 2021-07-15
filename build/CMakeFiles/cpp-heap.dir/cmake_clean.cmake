dc909ce2dd19bb9d9f25043833289c88688db084
Tom <elunico@users.noreply.github.com>
2021-07-15 18:23:36 -0400

    Value semantics

---
+++ build/CMakeFiles/cpp-heap.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp-heap.dir/heap.cpp.o"
+  "CMakeFiles/cpp-heap.dir/heap.cpp.o.d"
+  "CMakeFiles/cpp-heap.dir/main.cpp.o"
+  "CMakeFiles/cpp-heap.dir/main.cpp.o.d"
+  "cpp-heap"
+  "cpp-heap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp-heap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
