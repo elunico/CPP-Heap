65bda722bb396149e827fc8fac6b8ec728024cf3
Tom <elunico@users.noreply.github.com>
2021-07-17 12:43:51 -0400

    Changed example in main.cpp

--- main.cpp
+++ main.cpp
@@ -13,40 +13,36 @@ void dump_heap(tom::heap<int> const& heap) {
 struct Vector3D {
   double x, y, z;
 
-  int operator>(Vector3D const& other) {
-    return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)) >
-                   sqrt(pow(other.x, 2) + pow(other.y, 2) + pow(other.z, 2))
-               ? 1
-               : -1;
-  }
+  double mag() const { return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)); }
 
-  int operator==(Vector3D const& other) {
-    return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)) ==
-           sqrt(pow(other.x, 2) + pow(other.y, 2) + pow(other.z, 2));
-  }
+  static Vector3D random();
 };
 
+Vector3D Vector3D::random() {
+  return Vector3D{rand() / (double)RAND_MAX, rand() / (double)RAND_MAX,
+                  rand() / (double)RAND_MAX};
+}
+
 int main() {
-  auto heap = tom::heap<Vector3D>::max_heap();
+  auto heap = tom::heap<Vector3D>{[](Vector3D self, Vector3D other) {
+    double mag_a = self.mag();
+    double mag_b = other.mag();
+    return mag_a == mag_b ? 0 : (mag_a > mag_b ? 1 : -1);
+  }};
 
   std::cout << "Size of a tom::heap " << sizeof(heap) << std::endl;
-  std::cout << "Size of a Vector3D " << sizeof(Vector3D) << std::endl;
+  std::cout << "Size of a Vector3D " << sizeof(Vector3D{}) << std::endl;
 
-  for (int i = 0; i < 500000; i++)
-    heap.put(Vector3D{rand() / (double)RAND_MAX, rand() / (double)RAND_MAX,
-                      rand() / (double)RAND_MAX});
+  for (int i = 0; i < 100000; i++)
+    heap.put(Vector3D::random());
 
-  // std::vector<int> v{};
-  // for (int i = 0; i < 500000; i++)
-  //   v.push_back(i);
+  while (heap.count() > 0) {
+    auto p = heap.pop();
+    if (p.mag() > 1.45) {
+      std::cout << p.mag() << std::endl;
+    }
+  }
 
-  //   size_t count = heap.count();
-
-  //   for (int i = 0; i < count; i++) {
-  //     auto a = heap.pop();
-  //     if (a % 351 == 0)
-  //       std::cout << a << " " << std::endl;
-  //   }
   std::cout << std::endl;
 
   return 0;
