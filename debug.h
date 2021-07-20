07e009f741f3618f5ba1cbc701f86c62baa723b9
Thomas Povinelli <tompov227@gmail.com>
2021-07-20 13:47:23 -0400

    debug.h has definitions for when -DNDEBUG is passed

--- debug.h
+++ debug.h
@@ -1,10 +1,10 @@
 #ifndef DEBUG_H
+
 #ifndef NDEBUG
 
 #include <iostream>
 #include <string>
 
-// std::string filename();
 #define filename                  \
   auto s = std::string(__FILE__); \
   auto idx = s.find_last_of('/'); \
@@ -15,16 +15,21 @@
   }
 
 #define DEBUG_PREFIX \
-  {                  \
-    filename std::cerr << "[DEBUG " << s << ":" << __LINE__ << "]: "
+  filename std::cerr << "[DEBUG " << s << ":" << __LINE__ << "]: "
 
-#define SHOW_EXPR(x)                             \
-  DEBUG_PREFIX << #x << " = " << x << std::endl; \
-  }
+#define SHOW_EXPR(x) \
+  { DEBUG_PREFIX << #x << " = " << x << std::endl; }
 
-#define SHOW_STR(v)                               \
-  DEBUG_PREFIX << "Message: " << #v << std::endl; \
-  }
+#define SHOW_STR(v) \
+  { DEBUG_PREFIX << "Message: " << #v << std::endl; }
 
-#endif
-#endif
+#else
+
+#define SHOW_STR(x) (void*)0;
+#define SHOW_EXPR(x) (void*)0;
+#define filename
+#define DEBUG_PREFIX
+
+#endif  // NDEBUG
+
+#endif  // DEBUG_H
